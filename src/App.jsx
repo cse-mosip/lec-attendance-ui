a304c7ac758ce138de7ffa974b1a71aa2a5a0794
AwesomeNipun <nipun1969@gmail.com>
2023-07-11 23:10:51 +0530

    Update folder structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Routers from './components/routers/Routers';
+
+function App() {
+  return (
+    <div className="page-container">
+      <div className="content-wrap">
+        <Navbar />
+        <Routers />
+      </div>
+    </div>
+  );
+}
+export default App;
