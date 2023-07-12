b0b1eb5eef34577406f690fac362259ee76bb725
Thiniraxz <thiniragenuka@gmail.com>
2023-07-12 12:40:45 +0530

    Add side nav with app bar

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFound from "../pages/invalid_prohibited/NotFound";
 import Dashboard from "../pages/lecturer-view/dashboard/Dashboard";
+import SideNav from "../components/navbar/SideNav";
 
 function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
+    <>
+      <SideNav />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
 
-        {/* Invalid && prohibited routes  */}
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
-    </BrowserRouter>
+          {/* Invalid && prohibited routes  */}
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
