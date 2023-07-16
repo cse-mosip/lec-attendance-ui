01de0a4e5f18ca438ee5ce5ec14d012174731454
hesh-git <theshan.19@cse.mrt.ac.lk>
2023-07-16 12:28:11 +0530

    Edited app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import './App.css';
-import CustomizedTables from './components/dashboard-table/dashboard-table';
-// import Navbar from './components/navbar/Navbar';
-// import Routers from './components/routers/Routers';
-import DashboardCard from "./components/card/dashboardCard";
-import DashboardTable from "./components/dashboard-table/dashboard-table";
+import Navbar from './components/navbar/Navbar';
+import Routers from './components/routers/Routers';
 
 function App() {
   return (
     <div className="page-container">
       <div className="content-wrap">
-        <DashboardCard />
-        <DashboardTable />
+         <Navbar />
+         <Routers />
       </div>
     </div>
   );
