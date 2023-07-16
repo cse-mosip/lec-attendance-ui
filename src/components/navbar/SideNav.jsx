0538f8cdf624e4d078a53fc79f7f996ccdfdaaa1
nipun thejan <76776959+nipun-thejan@users.noreply.github.com>
2023-07-17 01:35:19 +0530

    lecture config removed from sidebar

--- src/components/navbar/SideNav.jsx
+++ src/components/navbar/SideNav.jsx
@@ -286,37 +286,6 @@ export default function MiniDrawer() {
         <Divider />
         <List>
           <Typography sx={{ color: "#899BBD", ml: 1 }}>Analyze</Typography>
-          {["Lecture Config"].map((text, index) => (
-            (text.toLowerCase() == "lecture config" && !isLoggedIn) ? null : (
-              <ListItem key={text} disablePadding sx={{ display: "block" }}>
-                <ListItemButton
-                  sx={{
-                    minHeight: 48,
-                    justifyContent: open ? "initial" : "center",
-                    px: 2.5,
-                    color: iconColor,
-                    //   backgroundColor: "#F0F4FB",
-                  }}
-                >
-                  <ListItemIcon
-                    sx={{
-                      minWidth: 0,
-                      mr: open ? 3 : "auto",
-                      justifyContent: "center",
-                      color: iconColor,
-                    }}
-                  >
-                    {text.toLowerCase() === "lecture config" ? (
-                      <SettingsIcon 
-                      onClick={() => window.location.href = "lecture-config"}/>
-                    ) : null}
-                  </ListItemIcon>
-                  <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-                </ListItemButton>
-              </ListItem>
-            )
-
-          ))}
           {["Dashboard", "Logout"].map((text, index) => (
             (text.toLowerCase() == "logout" && !isLoggedIn) ? null : (
               <ListItem key={text} disablePadding sx={{ display: "block" }}>
