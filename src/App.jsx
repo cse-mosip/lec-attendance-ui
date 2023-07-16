afced60f4012366ff4c164d1750cc355723dfb4a
ThevinSenath <thevinsenath12345@gmail.com>
2023-07-16 11:53:39 +0530

    dashboard_table component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/navbar/Navbar';
-import Routers from './components/routers/Routers';
+import CustomizedTables from './components/dashboard-table/dashboard-table';
+// import Navbar from './components/navbar/Navbar';
+// import Routers from './components/routers/Routers';
 
 function App() {
   return (
     <div className="page-container">
       <div className="content-wrap">
-        <Navbar />
-        <Routers />
+        {/* <Navbar /> */}
+        <CustomizedTables/>
+        {/* <Routers /> */}
       </div>
     </div>
   );
