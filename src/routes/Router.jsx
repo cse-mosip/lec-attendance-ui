8c22a35d2ea8fdf81617bdbb04d55c8a7553ce0c
KavishkaGunathilaka <gihanthakavishka@gmail.com>
2023-07-16 22:46:52 +0530

    intergrated student display with lecture config and chnaged sidenav

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -6,16 +6,17 @@ import SideNav from "../components/navbar/SideNav";
 import LectureConfig from "../pages/admin-student-view/admin-config-page/lecture-config";
 import AttendenceSheet from "../pages/lecturer-view/attendence-sheet/attendence-sheet";
 import Login from "../pages/login/login";
+import StudentDisplay from "../pages/admin-student-view/student-view/student";
 
 function Router() {
   return (
     <>
-      <SideNav />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/lecture-config" element={<LectureConfig />} />
           <Route path="/attendence-sheet" element={<AttendenceSheet />} />
+          <Route path="/student-view" element={<StudentDisplay />} />
           {/* Invalid && prohibited routes  */}
           <Route path="*" element={<NotFound />}></Route>
           <Route path="/login" element={<Login />}></Route>
