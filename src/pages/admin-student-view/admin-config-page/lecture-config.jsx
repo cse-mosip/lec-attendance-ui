64a1bc9a61211add733defb135bb12306cbf2894
Deshan-Lakshitha <deshanlakshithawickramasinghe@gmail.com>
2023-08-10 18:34:25 +0530

    Fixed errors

--- src/pages/admin-student-view/admin-config-page/lecture-config.jsx
+++ src/pages/admin-student-view/admin-config-page/lecture-config.jsx
@@ -18,7 +18,7 @@ import axios from "axios";
 import ErrorSnackbar from "./error-snackbar";
 import BasicTimeField from "./basic-time-field";
 import { useNavigate } from "react-router-dom";
-import SideNav from "../../../components/navbar/AdminSideNav";
+import AdminSideNav from "../../../components/navbar/AdminSideNav";
 import Swal from "sweetalert2";
 import { getAllHalls } from "../../../services/AdminServices";
 
