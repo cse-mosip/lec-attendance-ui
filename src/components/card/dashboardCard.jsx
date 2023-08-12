d0faf9483e594ce874b7229f01cfa489e83b5f78
Ravin Dulanjana Fernando <ravin.19@cse.mrt.ac.lk>
2023-08-13 00:21:06 +0530

    dashboard_card attendance button ui updated

--- src/components/card/dashboardCard.jsx
+++ src/components/card/dashboardCard.jsx
@@ -10,43 +10,52 @@ import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
 export default function PricingCards() {
     return (
-        <Box
-            sx={{
-                width: '100%',
-                display: 'grid',
-                gridTemplateColumns: 'repeat(auto-fill, minmax(min(100%, 300px), 1fr))',
-                gap: 2,
-                p: 5,
-                pb: 0
-            }}
-        >
-            <Card size="md" variant="outlined">
-                <Chip size="sm" variant="outlined" color="success">
-                    ONGOING
-                </Chip>
-                <Typography level="h2" fontSize="xl2">
-                    Professional Practice | CS 3032
-                </Typography>
-                {/*<Typography level="h4" fontSize="xl1">*/}
-                {/*    CS 3032*/}
-                {/*</Typography>*/}
-                <Divider inset="none" />
-                <CardActions>
-                    <Button
-                        variant="soft"
-                        color="neutral"
-                        endDecorator={<KeyboardArrowRight />}
-                    >
-                        Attendance
-                    </Button>
-                    <Typography level="h2" fontWeight="bold" sx={{ mr: 'auto' }} color="success">
-                        148{' '}
-                        <Typography fontSize="sm" textColor="text.tertiary">
-                            / 160
-                        </Typography>
-                    </Typography>
-                </CardActions>
-            </Card>
-        </Box>
+      <Box
+        sx={{
+          width: "100%",
+          display: "grid",
+          gridTemplateColumns:
+            "repeat(auto-fill, minmax(min(100%, 300px), 1fr))",
+          gap: 2,
+          p: 5,
+          pb: 0,
+        }}
+      >
+        <Card size="md" variant="outlined">
+          <Chip size="sm" variant="outlined" color="success">
+            ONGOING
+          </Chip>
+          <Typography level="h2" fontSize="xl2">
+            Professional Practice | CS 3032
+          </Typography>
+          {/*<Typography level="h4" fontSize="xl1">*/}
+          {/*    CS 3032*/}
+          {/*</Typography>*/}
+          <Divider inset="none" />
+          <CardActions>
+            <Button
+              variant="soft"
+              color="neutral"
+              endDecorator={<KeyboardArrowRight />}
+              style={{
+                pointerEvents: "none" 
+              }}
+            >
+              Attendance
+            </Button>
+            <Typography
+              level="h2"
+              fontWeight="bold"
+              sx={{ mr: "auto" }}
+              color="success"
+            >
+              148{" "}
+              <Typography fontSize="sm" textColor="text.tertiary">
+                / 160
+              </Typography>
+            </Typography>
+          </CardActions>
+        </Card>
+      </Box>
     );
 }
