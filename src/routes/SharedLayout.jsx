bf89ad4d0a06d138fa8852ffb9de3ccebf264e1c
Ravin Dulanjana Fernando <ravin.19@cse.mrt.ac.lk>
2023-08-13 03:04:49 +0530

    Update sidebar and page layout.

--- src/routes/SharedLayout.jsx
+++ src/routes/SharedLayout.jsx
@@ -1,20 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import SideNav from "../components/navbar/SideNav";
 import Navbar from '../components/navbar/Navbar';
+import './SharedLayout.css'
 
 const SharedLayout = () => {
   return (
-    <div>
-      <main className='dashboard'>
-        <Navbar />
+    <div className='dashboard'>
+      <Navbar />
+      <div className='dashboard-content'>
         <SideNav />
-        <div>
-          {/* <Navbar /> */}
-          <div className='dashboard-page'>
-            <Outlet />
-          </div>
+        <div className='dashboard-page'>
+          <Outlet />
         </div>
-      </main>
+      </div>
     </div>
   )
 }
