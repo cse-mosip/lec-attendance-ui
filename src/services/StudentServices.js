5961ab0121904116ab7f22b6b6259d6ec20e6229
KavishkaGunathilaka <gihanthakavishka@gmail.com>
2023-08-15 13:53:10 +0530

    added button to scan fingerprint

--- src/services/StudentServices.js
+++ src/services/StudentServices.js
@@ -1,12 +1,18 @@
 import axios from "axios";
+import { DOMAIN_NAME } from "../config.js";
 
-const BackendAPIEndpoint = process.env.REACT_APP_API_ENDPOINT + "/student";
+const accessToken = sessionStorage.getItem("AccessToken")
+
+// const BackendAPIEndpoint = process.env.REACT_APP_API_ENDPOINT + "/student";
+const BackendAPIEndpoint = DOMAIN_NAME + "/student";
 
 const markAttendance = async (data) => {
     const url = BackendAPIEndpoint + '/lecture-attendance/mark-lecture-attendance';
     const requestData = data;
     console.log(requestData);
-    const response = await axios.post(url, requestData);
+    const response = await axios.post(url, requestData, {headers: {
+        'Access-Token': accessToken
+    }});
     return response;
 }
 
